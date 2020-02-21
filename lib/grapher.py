c2cac12d9d74d725256516c8b14a743679b83eb5
Vishnu Namboodiri <vishnunamboodiri@Vishnus-MacBook-Air.local>
2020-02-21 10:22:32 +0530

    main.sh

--- lib/grapher.py
+++ lib/grapher.py
@@ -1,3 +1,33 @@
 #!/bin/python
 
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+# for filename in os.listdir("dat/tables"):
+# 	if filename.endswith(".csv"):
+# 		file=os.path.join("dat/tables/", filename)
+#         print(file)
+#          for line in file:
+#          	a = numpy.fromstring(line.strip(), dtype=int, sep=",")
+# #         data = np.genfromtxt(file, dtype=float, delimiter=',', names=True)
+#  #        print(data[1][1])
+
+#          continue
+#      else:
+#          continue
+
 print ("GRAPH")
+
+
+for filename in os.listdir("dat/tables"):
+	if filename.endswith(".csv"):
+		file=os.path.join("dat/tables/", filename)
+		print(file)
+		for line in file:
+			a = np.fromstring(line.strip(), dtype=int, sep=",")
+			print a
+		continue
+	else:
+		continue
--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 
 source reportinator.conf
 
-while getopts ":a:h:c" opt; do
+while getopts ":ahc" opt; do
   case $opt in
     h)
       echo "Reportinator 2.0"
@@ -25,13 +25,23 @@ while getopts ":a:h:c" opt; do
   esac
 done
 
+if [[ $TEX_RECONFIG == "yes" ]]; then
+	echo "Peforming initial setup..."
+	sleep 1
+	bash lib/install.sh
+	echo "Initialization complete! You're ready to use Reportinator now."
+	echo "Press any key"
+	read -n 1
+	exit
+fi
 
 PROJECT_FOLDER=$TEX_ROOT$(ls $TEX_ROOT | fzf --prompt="Choose Project: ")
 
+cp -r lib/dat-template lib/dat
 
 if [[ "$(ls $PROJECT_FOLDER)" == *.csv* ]]; then
 	echo "Found " $(ls $PROJECT_FOLDER | grep -o .csv  | wc -l) " csv files"
-	cp $PROJECT_FOLDER/*.csv dat/tables/
+	cp $PROJECT_FOLDER/*.csv lib/dat/tables/
 else
 	read -p "No csv files found. Do you wish to proceed anyway? (y,N)" yn
     case $yn in
@@ -45,29 +55,31 @@ if [[ "$(ls $PROJECT_FOLDER | grep -o .tex  | wc -l)" -eq 0 ]]; then
 	echo "No template file found. Using built-in template."
 elif [[ "$(ls $PROJECT_FOLDER | grep -o .tex  | wc -l)" -eq 1 ]]; then
 	echo "Found a template file." 
-	cp $PROJECT_FOLDER/*.tex dat/template.tex
+	cp $PROJECT_FOLDER/*.tex lib/dat/template.tex
 else 
 	TEMPLATE=$(ls $PROJECT_FOLDER | grep .tex | fzf --header="Found more than one tex file. Choose one to use as template.")
-	cp $PROJECT_FOLDER/$TEMPLATE dat/template.tex
+	cp $PROJECT_FOLDER/$TEMPLATE lib/dat/template.tex
 fi
 
-echo "" > dat/output.tex
+echo "" > lib/dat/output.tex
 while read -r line; do
 	if [[ "$line" == *"~observations~"* ]]; then
-		printf "%s\n" $(python lib/tabler.py) >> dat/output.tex
+		printf "%s\n" $(python lib/tabler.py) >> lib/dat/output.tex
 	elif [[ "$line" == "*~graphs~*" ]]; then
-		printf "%s\n" $(python lib/grapher.py) >> dat/output.tex
+		printf "%s\n" $(python lib/grapher.py) >> lib/dat/output.tex
 	elif [[ "$line" == "*~errors~*" ]]; then
-		printf "%s\n" $(python lib/errorer.py) >> dat/output.tex	
+		printf "%s\n" $(python lib/errorer.py) >> lib/dat/output.tex	
 	else
-		printf "%s\n" "$line" >> dat/output.tex
+		printf "%s\n" "$line" >> lib/dat/output.tex
 
 	fi
-done <dat/template.tex
+done <lib/dat/template.tex
 
 printf 'hello\r'
 
-
+echo "Process Complete, opening TeX file..."
+sleep 0.3
+$TEX_EDITOR lib/dat/output.tex 
 
 
 
